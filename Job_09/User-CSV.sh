c640d8c83fd91165c87e419ee87c9ddea8aad457
rodrigue-delogu <114388360+rodrigue-delogu@users.noreply.github.com>
2022-10-07 21:33:13 +0200

    Update User-CSV.sh

--- Job_09/User-CSV.sh
+++ Job_09/User-CSV.sh
@@ -18,7 +18,7 @@ do
 
 #Créa user 
 	sudo adduser ${rec_column1,,} --gecos "$rec_column1 $rec_column2 ,,," --disabled-password
-    sudo adduser ${rec_column1,,} --shell /bin/false --home /home/{rec_column1,,}
+	sudo adduser ${rec_column1,,} --shell /bin/false --home /home/{rec_column1,,}
 	echo "${rec_column1,,}:$rec_column3" | sudo chpasswd
 
 #Groupe Admin 	
